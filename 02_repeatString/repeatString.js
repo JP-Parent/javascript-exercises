64b4369eccfa27f6045193b36d457cac1af9e855
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-24 17:43:31 -0400

    added code for test and uncommented tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (word === null){
+        return Error;
+    }
+    else if (times < 0){
+        return Error;
+    }
 
+    else {
+        var string = "";
+        for (var i=0; i < times; i++){
+            string += word;
+        }
+        return string;
+    }
 };
 
 // Do not edit below this line
