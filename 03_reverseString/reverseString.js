dbcf1d88742846ef4c94a964daf266fac1c6706d
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-25 11:01:48 -0400

    added code to make tests work

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var reverserString = "";
+    for(var i = string.length -1; i >= 0; i--){
+        reverserString += string[i];
+    }
+    return reverserString;
 };
 
 // Do not edit below this line
