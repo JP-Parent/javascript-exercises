6f72d3783a7e87305cd940ead1521c24670e4c69
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-25 11:54:13 -0400

    added code to make tests work

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1)
+        || !Number.isInteger(num2)
+        || num1 < 0
+        || num2 < 0) 
+    return "ERROR"
 
+    else 
+    var smallNum = num1;
+    var bigNum = num2;
+    var result = 0;
+    if(num1 > num2){
+        bigNum = num1;
+        smallNum = num2;
+    }
+    for(var i = smallNum; i <= bigNum; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
