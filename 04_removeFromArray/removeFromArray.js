8956ba8d52139841dd341c764514ed12143475a2
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-25 11:40:14 -0400

    added array and spread arguments to func to make tests work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+   var newArray = array.filter(x => !args.includes(x))
+
+   return newArray;
 };
 
 // Do not edit below this line
