aae4210e3bed03781a080edc32e820976891f293
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-25 11:59:04 -0400

    refac code test works

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,14 @@
-const sumAll = function(num1, num2) {
-    if (!Number.isInteger(num1)
-        || !Number.isInteger(num2)
-        || num1 < 0
-        || num2 < 0) 
-    return "ERROR"
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"
+    if (min < 0 || max < 0) return "ERROR"
 
-    else 
-    var smallNum = num1;
-    var bigNum = num2;
-    var result = 0;
-    if(num1 > num2){
-        bigNum = num1;
-        smallNum = num2;
+    var sum = 0;
+    if(min > max) [min, max] = [max, min];
+
+    for(var i = min; i <= max; i++){
+        sum += i;
     }
-    for(var i = smallNum; i <= bigNum; i++){
-        result += i;
-    }
-    return result;
+    return sum;
 };
 
 // Do not edit below this line
