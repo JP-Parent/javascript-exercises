bffafea2e7c355bf3a81712d8d6a9aaab1fb4103
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-28 19:19:16 -0400

    refac palindrome code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,29 @@
-const palindromes = function (string) {
-    string = string.toLowerCase();
-    var punctuationLess = string.replace(/[!,." "]/g, "");
-    var stringArray = [];
-    for (var i = 0; i < punctuationLess.length; i++) {
-        stringArray.push(punctuationLess.charAt(i));
-    }
+// const palindromes = function (string) {
+//     string = string.toLowerCase();
+//     var punctuationLess = string.replace(/[!,." "]/g, "");
+//     var stringArray = [];
+//     for (var i = 0; i < punctuationLess.length; i++) {
+//         stringArray.push(punctuationLess.charAt(i));
+//     }
 
-    var reverseArray = stringArray.toReversed();
-    for (var i = 0; i < stringArray.length; i++) {
-        if (stringArray[i] !== reverseArray[i]) return false;
-    }
-    return true;
+//     var reverseArray = stringArray.toReversed();
+//     for (var i = 0; i < stringArray.length; i++) {
+//         if (stringArray[i] !== reverseArray[i]) return false;
+//     }
+//     return true;
+// };
+
+const palindromes = function (string) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890"
+
+    var goodString = string
+    .toLowerCase()
+    .split("")
+    .filter((x) => alphanumeric.includes(x))
+    .join("");
+
+    var reverseString = goodString.split("").reverse().join("");
+    return goodString === reverseString;
 };
 
 // Do not edit below this line
