c9bad806d7df00c5c7277347c29bf73aaaf53bc7
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-28 17:36:37 -0400

    added code for add, sub, sum and multiply test to work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,17 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.length ? array.reduce((total, currentNum) => total + currentNum) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, currentNum) => total * currentNum);
 };
 
 const power = function() {
