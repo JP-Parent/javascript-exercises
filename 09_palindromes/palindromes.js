e1ed2eba2910b6431ccf76c25eff5ad9e0194ae0
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-28 19:07:38 -0400

    added code for palindrome tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    var punctuationLess = string.replace(/[!,." "]/g, "");
+    var stringArray = [];
+    for (var i = 0; i < punctuationLess.length; i++) {
+        stringArray.push(punctuationLess.charAt(i));
+    }
 
+    var reverseArray = stringArray.toReversed();
+    for (var i = 0; i < stringArray.length; i++) {
+        if (stringArray[i] !== reverseArray[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
