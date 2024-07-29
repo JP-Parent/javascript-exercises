7533afc6e9e5dd3b8125f2cf64781d1ed0e224a5
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-29 12:40:17 -0400

    added function to get age + code to make test pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    var oldestPerson = people.reduce((oldest, person) => {
+        const age = getAge(person.yearOfBirth, person.yearOfDeath);
+        return age > getAge(oldest.yearOfBirth, oldest.yearOfDeath) ? person : oldest;
+    });
+    return oldestPerson;
 
 };
 
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 // Do not edit below this line
 module.exports = findTheOldest;
