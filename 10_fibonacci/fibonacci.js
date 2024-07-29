c0bfdfc327e40c677bc7ab35bbad48ad99efd801
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-29 10:29:12 -0400

    added code for fibonacci tests to work

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    const fib = [0,1];
+    var count;
+    
+    if(typeof a !== "number"){
+    	count = parseInt(a);
+    }
+    else count = a
 
+    if (count < 0) return "OOPS"
+
+    for(var i = 2; i <= count; i++){
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib[count];
 };
 
 // Do not edit below this line
